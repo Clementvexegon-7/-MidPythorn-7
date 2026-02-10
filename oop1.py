c1359440d744b4314dda0512db8bde87964dbc0b
Clementvexegon-7 <Clementvexegon@gmail.com>
2026-02-10 12:49:20 +0300

    initial commit

--- oop1.py
+++ oop1.py
@@ -4,3 +4,16 @@ class Dog:
         self.name = name
         self.breed = breed
         self.hasFur = hasFur
+
+
+    def bark(self):
+        print("Woof!! Woof!!")
+
+    def locomotive(self):
+        print("Dog is walking")
+
+dog1 = Dog("JJ", "Bulldog", "True")
+
+dog2 = Dog("Tony", "German Shephard", "True")
+
+dog3 = Dog("Ann", "Siberian Husky", "True")
